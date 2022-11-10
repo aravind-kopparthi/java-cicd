f846f73db62bbf5536efdccd27be12c7234c374d
Satya Aravind Kopparthi <36413278+aravind-kopparthi@users.noreply.github.com>
2022-11-10 11:32:50 -0500

    Create tag-version-gen.sh

---
+++ tag-version-gen.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#get highest tag number that starts with v
+#Script that will find the last Git Tag and increment it. It will only increment when the latest commit does not already have a tag. 
+#By default it increments the patch number, you can tell it to change the major or minor versions by adding #major or #minor to the commit message.
+#git tag --list --sort=-version:refname "v*" | head -n 1
+#git describe --match "v[0-9]*" --abbrev=4 HEAD
+# git ls-remote --tags origin  
+#VERSION=`git describe --match "RC-[0-9]*" --abbrev=0 --tags`
+VERSION=`git describe --match "v[0-9].[0-9].[0-9]" --abbrev=0 --tags`
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    VNUM1=0
+    VNUM2=0
+    VNUM3=0
+else
+    #replace . with space so can split into an array
+    VERSION_BITS=(${VERSION//./})
+
+    
+    #get number parts and increase last one by 1
+    VNUM1=${VERSION_BITS[0]}
+    VNUM2=${VERSION_BITS[1]}
+    VNUM3=${VERSION_BITS[2]}
+
+    VNUM1=`echo $VNUM1 | sed 's/v//'`
+fi
+
+
+
+
+echo "------major-----"
+echo "$VNUM1"
+echo "------minor-----"
+echo "$VNUM2"
+echo "------patch-----"
+echo "$VNUM3"
+# Check for #major or #minor in commit message and increment the relevant version number
+MAJOR=`git log --format=%B -n 1 HEAD | grep '#major'`
+MINOR=`git log --format=%B -n 1 HEAD | grep '#minor'`
+
+if [ "$MAJOR" ]; then
+    echo "Update major version"
+    VNUM1=$((VNUM1+1))
+    VNUM2=0
+    VNUM3=0
+elif [ "$MINOR" ]; then
+    echo "Update minor version"
+    VNUM2=$((VNUM2+1))
+    VNUM3=0
+else
+    echo "Update patch version"
+    VNUM3=$((VNUM3+1))
+fi
+
+
+
+#create new tag
+NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+
+echo "Updating $VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+
+#only tag if no tag already (would be better if the git describe command above could have a silent option)
+if [ -z "$NEEDS_TAG" ]; then
+    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+  #  git tag $NEW_TAG
+  #  git push --tags
+else
+    echo "Already a tag on this commit"
+fi
